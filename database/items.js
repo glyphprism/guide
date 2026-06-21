e627391b5a97aba87dc55884d449b500ff8dac12
glyphprism <glyphprism@gmail.com>
2026-06-22 00:09:36 +0700

    Create items.js

---
+++ database/items.js
@@ -0,0 +1,11 @@
+const items = [
+  {
+    id: 1,
+    nama: "Parfum",
+    foto: "image/items/parfum.jpg",
+    catatan: "Dapat meningkatkan hubungan baik dengan beberapa warga jika diberikan sebagai hadiah",
+    caraMendapatkan: [
+      "Beli ditoko pak tatang"
+    ]
+  }
+];
